d43eedd2eb411058d7e3a47325341a841e486053
Antonio <antoniocrespgome@gmail.com>
2021-11-23 13:49:35 +0100

    añadido c y c++

---
+++ Ej7/c++/antonio.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hola, soy Antonio Crespo \n";
+	return 0;
+}
---
+++ Ej7/c/antonio.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola, soy Antonio Crespo");
+    return 0;
+}
